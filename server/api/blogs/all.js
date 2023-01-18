6842d09d647c7d29381388bc87cb360d762bbf80
unknown <mostafa.e@gogoprint.com.my>
2023-01-19 02:56:04 +0800

    update all blogs api to get limited randomly data

--- server/api/blogs/all.js
+++ server/api/blogs/all.js
@@ -1,5 +1,7 @@
 import Blog from "../../models/blog";
 export default defineEventHandler(async () => {
-  const allBlogs = await Blog.find();
+  // select randomly blogs to appear in the main page
+  const allBlogs = await Blog.aggregate([{ $sample: { size: 15 } }]).exec();
+
   return allBlogs;
 });
