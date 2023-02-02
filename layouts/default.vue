330b5433bc265ff875a61bc548a73f260b6a0a8f
unknown <mostafa.e@gogoprint.com.my>
2023-02-02 18:55:38 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <meta name="google-site-verification" content="googlee803e2020fb0b755.html" />
+  <div>
+    <meta
+      name="google-site-verification"
+      content="googlee803e2020fb0b755.html"
+    />
+  </div>
 
   <Header />
   <div>
